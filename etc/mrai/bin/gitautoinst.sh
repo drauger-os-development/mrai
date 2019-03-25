9b4726ba1a4648e581e6e6b64690243e7a84cf86
Thomas Castleman <thomas.castleman2019@gmail.com>
2019-03-25 12:15:30 -0400

    fixed bug and updated email
    when passing a literal string to mrai under search function, would throw an error

--- etc/mrai/bin/gitautoinst.sh
+++ etc/mrai/bin/gitautoinst.sh
@@ -3,7 +3,7 @@
 #
 #  gitautoinst.sh
 #  
-#  Copyright 2019 Thomas Castleman <contact@draugeros.ml>
+#  Copyright 2019 Thomas Castleman <contact@draugeros.org>
 #  
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
