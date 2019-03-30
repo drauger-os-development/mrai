590784c396286d278c3fbaa1f79f64349621f6cc
Thomas Castleman <thomas.castleman2019@gmail.com>
2019-03-30 02:25:02 -0400

    Added ability to edit sources.list files from apt
    Expands upon apt's capablities by allowing the user to edit any of the *.list files. Not just sources.list, but also any of the files in sources.list.d

---
+++ etc/mrai/bin/edit-apt-sources.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+#
+#  edit-apt-sources.sh
+#  
+#  Copyright 2019 Thomas Castleman <contact@draugeros.org>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+#VERSION 0.0.1-alpha1
+set -e
+set -o pipefail
+file="$1"
+usr=$(/usr/bin/users)
+cache="/etc/mrai"
+int=0
+while [ "$int" == "0" ]; then
+    if [ -f /home/$usr/.selected_editor ] || [ -f $cache/selected_editor.conf ]; then
+        if [ ! -f /home/$usr/.selected_editor ] && [ -f $cache/selected_editor.conf ]; then
+            editor=$(/bin/grep $cache/selected_editor.conf | /bin/sed 's/SELECTED_EDITOR=//g')
+
+        elif [ -f /home/$usr/.selected_editor ] && [ ! -f $cache/selected_editor.conf ]; then
+            editor=$(/bin/grep /home/$usr/.selected_editor | /bin/sed 's/SELECTED_EDITOR=//g')
+            /bin/cp /home/$usr/.selected_editor $cache/selected_editor.conf
+            /bin/chown "$usr":"$usr" $cache/selected_editor.conf
+        else
+            exit 2
+        fi
+        int="1"
+    else
+        /usr/bin/select-editor
+    fi
+done
+{
+    /bin/cp $file "$file.back_up_mrai"
+    eval "$editor $file"
+} && {
+    /bin/echo -e "\nUpdating Apt Repository Cache . . .\n"
+    /usr/bin/apt update
+} && {
+    /bin/echo -e "\nConfiguring Apt Repos Complete!\n"
+}
