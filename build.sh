0aff30ff24aadafa76f81d61e375ff8cd97fdd0b
Thomas Castleman <thomas.castleman2019@gmail.com>
2019-04-14 21:13:43 -0400

    build procedure update
    build.sh will now compress necessary files for you

--- build.sh
+++ build.sh
@@ -7,5 +7,10 @@ cp -R etc ../"$PAK"_"$VERSION"_all/etc
 cp -R usr ../"$PAK"_"$VERSION"_all/usr
 cp -R DEBIAN ../"$PAK"_"$VERSION"_all/DEBIAN
 cd ..
+gzip "$PAK"_"$VERSION"_all/usr/share/doc/$PAK/changelog
+cd "$PAK"_"$VERSION"_all/usr/share/man/man1
+gzip $(ls)
+cd ../../../../..
+sleep 0.5s
 dpkg-deb --build "$PAK"_"$VERSION"_all
 rm -rf "$PAK"_"$VERSION"_all
