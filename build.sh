36593161644c34bd1e15af8d6d8a5b0d955e122e
Thomas Castleman <thomas.castleman2019@gmail.com>
2019-07-14 17:34:32 -0400

    fix packaging bugs
    Now that we have some C++ in the package, we have to compile it. Furthermore, this compiled code will only run on processors it is compiled for, so we need to mark that in the package. That's what all this commit handles.

--- build.sh
+++ build.sh
@@ -1,16 +1,84 @@
 #!/bin/bash
 VERSION=$(cat DEBIAN/control | grep 'Version: ' | sed 's/Version: //g')
 PAK=$(cat DEBIAN/control | grep 'Package: ' | sed 's/Package: //g')
-mkdir ../"$PAK"_"$VERSION"_all
-cp -R bin ../"$PAK"_"$VERSION"_all/bin
-cp -R etc ../"$PAK"_"$VERSION"_all/etc
-cp -R usr ../"$PAK"_"$VERSION"_all/usr
-cp -R DEBIAN ../"$PAK"_"$VERSION"_all/DEBIAN
+ARCH=$(cat DEBIAN/control | grep 'Architecture: '| sed 's/Architecture: //g')
+FOLDER="$PAK\_$VERSION\_$ARCH"
+FOLDER=$(echo "$FOLDER" | sed 's/\\//g')
+mkdir ../"$FOLDER"
+##############################################################
+#							     #
+#							     #
+#  COMPILE ANYTHING NECSSARY HERE			     #
+#							     #
+#							     #
+##############################################################
+cd usr/share/mrai
+g++ -Wall -m64 -o "log-out" "log-out.cxx"
+cd ../..
+##############################################################
+#							     #
+#							     #
+#  REMEMBER TO DELETE SOURCE FILES FROM TMP		     #
+#  FOLDER BEFORE BUILD					     #
+#							     #
+#							     #
+##############################################################
+if [ -d bin ]; then
+	cp -R bin ../"$FOLDER"/bin
+fi
+if [ -d etc ]; then
+	cp -R etc ../"$FOLDER"/etc
+fi
+if [ -d usr ]; then
+	cp -R usr ../"$FOLDER"/usr
+fi
+if [ -d lib ]; then
+	cp -R lib ../"$FOLDER"/lib
+fi
+if [ -d lib32 ]; then
+	cp -R lib32 ../"$FOLDER"/lib32
+fi
+if [ -d lib64 ]; then
+	cp -R lib64 ../"$FOLDER"/lib64
+fi
+if [ -d libx32 ]; then
+	cp -R libx32 ../"$FOLDER"/libx32
+fi
+if [ -d dev ]; then
+	cp -R dev ../"$FOLDER"/dev
+fi
+if [ -d home ]; then
+	cp -R home ../"$FOLDER"/home
+fi
+if [ -d proc ]; then
+	cp -R proc ../"$FOLDER"/proc
+fi
+if [ -d root ]; then
+	cp -R root ../"$FOLDER"/root
+fi
+if [ -d run ]; then
+	cp -R run ../"$FOLDER"/run
+fi
+if [ -d sbin ]; then
+	cp -R sbin ../"$FOLDER"/sbin
+fi
+if [ -d sys ]; then
+	cp -R sys ../"$FOLDER"/sys
+fi
+if [ -d tmp ]; then
+	cp -R tmp ../"$FOLDER"/tmp
+fi
+if [ -d var ]; then
+	cp -R var ../"$FOLDER"/var
+fi
+if [ -d opt ]; then
+	cp -R opt ../"$FOLDER"/opt
+fi
+if [ -d srv ]; then
+	cp -R srv ../"$FOLDER"/srv
+fi
+cp -R DEBIAN ../"$FOLDER"/DEBIAN
 cd ..
-gzip "$PAK"_"$VERSION"_all/usr/share/doc/$PAK/changelog
-cd "$PAK"_"$VERSION"_all/usr/share/man/man1
-gzip $(ls)
-cd ../../../../..
-sleep 0.5s
-dpkg-deb --build "$PAK"_"$VERSION"_all
-rm -rf "$PAK"_"$VERSION"_all
+rm "$FOLDER"/usr/share/mrai/log-out.cxx
+dpkg-deb --build "$FOLDER"
+rm -rf "$FOLDER"
